ec4c43c2ba0b47851ca9c5cfd7339bbb87cd6298
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-09-11 19:13:24 -0300

    refactor: remove unused import from Piloto

--- src/main/java/model/Piloto.java
+++ src/main/java/model/Piloto.java
@@ -2,7 +2,6 @@ package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
